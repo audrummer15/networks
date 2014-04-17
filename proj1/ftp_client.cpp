2f07c7910bd917ac8dffb21bc35dd2f05270f89c
Adam Brown <audrummer15@users.noreply.github.com>
2014-04-17 09:16:19 -0500

    Update ftp_client.cpp

--- proj1/ftp_client.cpp
+++ proj1/ftp_client.cpp
@@ -91,110 +91,114 @@ int main(int argc, char *argv[])
         }
     }
 
-    //Open socket
-    if ((fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-        cerr << "Unable to open socket. Exiting..." << endl;
-        return 0;
-    }
-
-    bzero(&serverAddress, sizeof(serverAddress));
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(PORT);
-
-    if (inet_aton(sServerAddress.c_str(), &serverAddress.sin_addr)==0)
-    {
-        cerr << "There was a problem with the server IP address. Exiting..." << endl;
-        return 0;
-    }
-
-
     //Loop forever
     string ln;
     for(;;)
     {
-	cout << "Type command or quit to quit: ";
-	cin >> ln;
-	if(ln.compare("quit") == 0)
-		break;
-	if(ln.compare("PUT") == 0 || ln.compare("put") == 0)
-	{
-		cin >> ln;
-		//Now ln is our filename to send
-		ifstream putfile;
-		putfile.open(ln.c_str());
-		unsigned char csum = 0x00;
-		unsigned char lost = 0x00;
-		char buff[DATASIZE];
-		if(putfile.is_open())
+		//Open socket
+		if ((fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+			cerr << "Unable to open socket. Exiting..." << endl;
+			return 0;
+		}
+
+		bzero(&serverAddress, sizeof(serverAddress));
+		serverAddress.sin_family = AF_INET;
+		serverAddress.sin_port = htons(PORT);
+
+		if (inet_aton(sServerAddress.c_str(), &serverAddress.sin_addr)==0)
 		{
-			
-			//Now we send a 1 packet overhead for the filename
-			
-			bSent = false;
-			pPacket = constructPacket((char*)"PUT TestFile", strlen("PUT TestFile"));
-			while(!bSent ) {
-				memcpy(pTemp, pPacket, sizeof( Packet ));
-				bGremlin = gremlin(fDamaged, fLost, pTemp);
-				bSent = sendPacket(pTemp, bGremlin);
-				cout << "Sending put..." << endl;
-			}	
-			
-			while(!putfile.eof())
+			cerr << "There was a problem with the server IP address. Exiting..." << endl;
+			return 0;
+		}
+
+		cout << "Type command or quit to quit: ";
+		cin.clear();
+		cin >> ln;
+		if(ln.compare("quit") == 0)
+			break;
+		if(ln.compare("PUT") == 0 || ln.compare("put") == 0)
+		{
+			cin >> ln;
+			//Now ln is our filename to send
+			ifstream putfile;
+			putfile.open(ln.c_str());
+			unsigned char csum = 0x00;
+			unsigned char lost = 0x00;
+			char buff[DATASIZE];
+			if(putfile.is_open())
 			{
-
-				/************************************************/
-				// This part of the code reads in from the      //
-				// open file and fills up the data part of      //
-				// the packet. It also calculates the checksum. //
-				/************************************************/
-				for(int i = 0; i < DATASIZE; i++)
-				{
-					if(!putfile.eof())
-					{
-						buff[i] = putfile.get();
-					}
-					else
-					{
-						if( i != 0 )
-							buff[i-1]= '\0';
-						buff[i] = '\0';
-					}
-				}
-
-				
-
-				/*************************************************/
-				// This part of the code looks at the input      //
-				// parameters and determines if a packet should  //
-				// be simulated lost or damaged. This is also    //
-  				// known as the GREMLIN function   		 //
-				/*************************************************/
-
-				//Send
+			
+				//Now we send a 1 packet overhead for the filename
+			
 				bSent = false;
-				pPacket = constructPacket(buff, strlen(buff));
-				while( !bSent ) {
+				pPacket = constructPacket((char*)"PUT TestFile", strlen("PUT TestFile"));
+				while(!bSent ) {
 					memcpy(pTemp, pPacket, sizeof( Packet ));
 					bGremlin = gremlin(fDamaged, fLost, pTemp);
-					bSent = sendPacket(pTemp, bGremlin); 
-					cout << "sending packet..." << endl;
-				}
-				
-				
-			}
+					bSent = sendPacket(pTemp, bGremlin);
+					cout << "Sending put..." << endl;
+				}	
 			
-			sendto(fd, "\0", 1, 0, (struct sockaddr*)&serverAddress, slen);
+				while(!putfile.eof())
+				{
 
-			cout << "Sending Complete!\n";
-		}
-		else
-		{
-			cout << "Could not open file name.\n";
-		}
+					/************************************************/
+					// This part of the code reads in from the      //
+					// open file and fills up the data part of      //
+					// the packet. It also calculates the checksum. //
+					/************************************************/
+					for(int i = 0; i < DATASIZE; i++)
+					{
+						if(!putfile.eof())
+						{
+							if( i < 48 ) {
+								cout << buff[i];
+							}
+							buff[i] = putfile.get();
+						}
+						else
+						{
+							if( i != 0 ) {
+								buff[i-1]= '\0';
+							}
+							buff[i] = '\0';
+						}
+					}
+					cout << endl;
+				
 
-	}
+					/*************************************************/
+					// This part of the code looks at the input      //
+					// parameters and determines if a packet should  //
+					// be simulated lost or damaged. This is also    //
+	  				// known as the GREMLIN function   		 //
+					/*************************************************/
+
+					//Send
+					bSent = false;
+					pPacket = constructPacket(buff, strlen(buff));
+					while( !bSent ) {
+						memcpy(pTemp, pPacket, sizeof( Packet ));
+						bGremlin = gremlin(fDamaged, fLost, pTemp);
+						bSent = sendPacket(pTemp, bGremlin); 
+					}
+				
+				
+				}
+			
+				sendto(fd, "\0", 1, 0, (struct sockaddr*)&serverAddress, slen);
+
+				cout << "Sending Complete!\n";
+				putfile.close();
+			}
+			else
+			{
+				cout << "Could not open file name.\n";
+			}
+
+		}
+		close(fd);
     }
-
 
     return 0;
 }
@@ -369,17 +373,17 @@ bool sendPacket(const Packet* pPacket, bool bLost) {
 		cerr << "Error polling socket..." << endl;
 		bReturn = false;
 	} else if( recvPollVal == 0 ) {        //If timeout occurs
-		cerr << "Poll Timeout!..." << endl;
+		cerr << "Timeout... Lost Packet, Sequence Number - "  << (int)pPacket->Sequence << endl;
 		bReturn = false;
 	} else {
 		iLength = recvfrom(fd, recvline, MAXLINE, 0, (struct sockaddr*)&serverAddress, &slt);
 
 		if( recvline[0] == '1') {          //If ACK Received, return true
-			cout << "ACK - " << (int)recvline[1] - 30 << endl;
+			cout << "ACK - " << (int)recvline[1] << endl;
 			//cout << pPacket->Data << endl;
 		    bReturn = true;
 		} else if( recvline[0] == '0' ) {     //Else if NAK, return false
-			cout << "NAK - " << (int)recvline[1] - 30 << endl;
+			cout << "NAK - " << (int)recvline[1] << endl;
 		    bReturn = false;
 		} else {                             //Else bad stuff, return false
 		    bReturn = false;
