22bb96ddeefd93a3a980ed8efcd629d725488478
Adam Micheal Brown <amb0027@tux189.eng.auburn.edu>
2014-04-18 23:04:59 -0500

    Final Project

--- proj1/ftp_client.cpp
+++ proj1/ftp_client.cpp
@@ -8,6 +8,7 @@
 
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/poll.h>
 
 #include <arpa/inet.h>
 #include <netinet/in.h>
@@ -24,42 +25,41 @@ using namespace std;
 
 typedef struct {
     uint8_t Sequence;
-    char Checksum;
+    unsigned char Checksum;
     char Data[DATASIZE];
 } Packet;	
 
 void usage();
 char generateChecksum(char*, int);
+bool gremlin(float, float, Packet*);
 char* loadFileToBuffer();
 Packet* constructPacket(char*, int);
+bool sendPacket(const Packet*, bool); 
+
+int fd;
+int slen;
+socklen_t slt;
+struct sockaddr_in serverAddress; 
 
 int main(int argc, char *argv[])
 {
     //Socket variables
     struct hostent *hp;
-    struct sockaddr_in serverAddress; 
     unsigned char buf[BUFSIZE];
-    int fd;
-    int slen=sizeof(serverAddress);
-    socklen_t slt = sizeof(serverAddress);
+    slen = sizeof(serverAddress);
+    slt = sizeof(serverAddress);
 
     //Command line arguments
     string sServerAddress;
     float fDamaged = 0;
     float fLost = 0;
 
+	bool bSent = false;
+	bool bGremlin = false;
+
     //Sending variables
-    uint8_t iSequence = 0;
     Packet *pPacket;
-    bool bSent = false; //File has successfully sent
-
-    //Other
-    char* cFileBuffer = loadFileToBuffer();
-
-    if( cFileBuffer == NULL ) {
-        cerr << "Error reading file. Exiting..." << endl;
-        return 0;
-    }
+	Packet *pTemp = new Packet;
 
     for(int i=1;i < argc; i+= 2)
     {
@@ -91,52 +91,232 @@ int main(int argc, char *argv[])
         }
     }
 
-    //Open socket
-    if ((fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-        cerr << "Unable to open socket. Exiting..." << endl;
-        return 0;
-    }
-
-    bzero(&serverAddress, sizeof(serverAddress));
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(PORT);
-
-    if (inet_aton(sServerAddress.c_str(), &serverAddress.sin_addr)==0)
+    //Loop forever
+    string ln;
+    for(;;)
     {
-        cerr << "There was a problem with the server IP address. Exiting..." << endl;
-        return 0;
+		//Open socket
+		if ((fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+			cerr << "Unable to open socket. Exiting..." << endl;
+			return 0;
+		}
+
+		bzero(&serverAddress, sizeof(serverAddress));
+		serverAddress.sin_family = AF_INET;
+		serverAddress.sin_port = htons(PORT);
+
+		if (inet_aton(sServerAddress.c_str(), &serverAddress.sin_addr)==0)
+		{
+			cerr << "There was a problem with the server IP address. Exiting..." << endl;
+			return 0;
+		}
+
+		cout << "Type command or quit to quit: ";
+		cin.clear();
+		cin >> ln;
+		if(ln.compare("quit") == 0)
+			break;
+		if(ln.compare("PUT") == 0 || ln.compare("put") == 0)
+		{
+			cin >> ln;
+			//Now ln is our filename to send
+			ifstream putfile;
+			putfile.open(ln.c_str());
+			unsigned char csum = 0x00;
+			unsigned char lost = 0x00;
+			char buff[DATASIZE];
+			if(putfile.is_open())
+			{
+			
+				//Now we send a 1 packet overhead for the filename
+			
+				bSent = false;
+				pPacket = constructPacket((char*)"PUT TestFile", strlen("PUT TestFile"));
+				while(!bSent ) {
+					memcpy(pTemp, pPacket, sizeof( Packet ));
+					bGremlin = gremlin(fDamaged, fLost, pTemp);
+					bSent = sendPacket(pTemp, bGremlin);
+					cout << "Sending put..." << endl;
+				}	
+			
+				while(!putfile.eof())
+				{
+
+					/************************************************/
+					// This part of the code reads in from the      //
+					// open file and fills up the data part of      //
+					// the packet. It also calculates the checksum. //
+					/************************************************/
+					for(int i = 0; i < DATASIZE; i++)
+					{
+						if(!putfile.eof())
+						{
+							if( i < 48 ) {
+								cout << buff[i];
+							}
+							buff[i] = putfile.get();
+						}
+						else
+						{
+							if( i != 0 ) {
+								buff[i-1]= '\0';
+							}
+							buff[i] = '\0';
+						}
+					}
+					cout << endl;
+				
+
+					/*************************************************/
+					// This part of the code looks at the input      //
+					// parameters and determines if a packet should  //
+					// be simulated lost or damaged. This is also    //
+	  				// known as the GREMLIN function   		 //
+					/*************************************************/
+
+					//Send
+					bSent = false;
+					pPacket = constructPacket(buff, strlen(buff));
+					while( !bSent ) {
+						memcpy(pTemp, pPacket, sizeof( Packet ));
+						bGremlin = gremlin(fDamaged, fLost, pTemp);
+						bSent = sendPacket(pTemp, bGremlin); 
+					}
+				
+				
+				}
+			
+				sendto(fd, "\0", 1, 0, (struct sockaddr*)&serverAddress, slen);
+
+				cout << "Sending Complete!\n";
+				putfile.close();
+			}
+			else
+			{
+				cout << "Could not open file name.\n";
+			}
+
+		}
+		close(fd);
     }
 
-    pPacket = constructPacket((char*)"zzzzzzzzzzzzzzzz", strlen("zzzzzzzzzzzzzzzz"));
-
-    if (sendto(fd, pPacket, BUFSIZE, 0, (struct sockaddr*)&serverAddress, slen) == -1) {
-        cerr << "Problem sending data. Exiting..." << endl;
-        return 0;
-    }
-
-    while( !bSent ) {
-        int iLength = 0;
-        char recvline[MAXLINE + 1];
-
-        iLength = recvfrom(fd, recvline, MAXLINE, 0, (struct sockaddr*)&serverAddress, &slt);
-
-
-        bSent = true;
-    }
-
-	delete pPacket;
     return 0;
+}
+
+bool gremlin(float fDamaged, float fLost, Packet* ppacket)
+{
+	if(fLost > (1.0 * rand()) / (1.0 * RAND_MAX))
+	{
+		return true;
+	}
+
+	if(fDamaged > (1.0 * rand()) / (1.0 * RAND_MAX))
+	{
+		int numDamaged = rand() % 10;
+		int byteNum = rand() % BUFSIZE;
+		if(numDamaged == 9)
+		{
+			if(numDamaged > 1)
+			{
+				ppacket->Data[numDamaged-HEADERSIZE]+= 8;	
+			}
+			else if(numDamaged == 1)
+			{
+				ppacket->Checksum+= 8;
+			}
+			else
+			{
+				ppacket->Sequence+= 8;
+			}
+			
+			numDamaged = rand() % 10;
+			if(numDamaged > 1)
+			{
+				ppacket->Data[numDamaged-HEADERSIZE]+= 4;	
+			}
+			else if(numDamaged == 1)
+			{
+				ppacket->Checksum+= 4;
+			}
+			else
+			{
+				ppacket->Sequence+= 4;
+			}
+
+			numDamaged = rand() % 10;
+			if(numDamaged > 1)
+			{
+				ppacket->Data[numDamaged-HEADERSIZE]+= 2;	
+			}
+			else if(numDamaged == 1)
+			{
+				ppacket->Checksum+= 2;
+			}
+			else
+			{
+				ppacket->Sequence+= 2;
+			}
+		}
+		else if(numDamaged > 6)
+		{
+			if(numDamaged > 1)
+			{
+				ppacket->Data[numDamaged-HEADERSIZE]+= 8;	
+			}
+			else if(numDamaged == 1)
+			{
+				ppacket->Checksum+= 8;
+			}
+			else
+			{
+				ppacket->Sequence+= 8;
+			}
+
+			numDamaged = rand() % 10;
+			if(numDamaged > 1)
+			{
+				ppacket->Data[numDamaged-HEADERSIZE]+= 4;	
+			}
+			else if(numDamaged == 1)
+			{
+				ppacket->Checksum+= 4;
+			}
+			else
+			{
+				ppacket->Sequence+= 4;
+			}
+		}
+		else
+		{
+			if(numDamaged > 1)
+			{
+				ppacket->Data[numDamaged-HEADERSIZE]+=8;	
+			}
+			else if(numDamaged == 1)
+			{
+				ppacket->Checksum+=4;
+			}
+			else
+			{
+				ppacket->Sequence+= 2;
+			}
+		}
+
+	}
+	return false;
 }
 
 void usage() {
     cout << "Use the following syntax: \nproject1 -l <lost packets> -d <damaged packets>" << endl;
 }
 
-char generateChecksum( char* data, int length ) {
-    char retVal = 0x00;
+unsigned char generateChecksum( Packet* pPacket ) {
+    unsigned char retVal = 0x00;
 
-    for( int i=0; i < length; i++ ) {
-        retVal += *(data + i);
+    retVal = pPacket->Sequence;
+
+    for( int i=0; i < DATASIZE; i++ ) {
+        retVal += pPacket->Data[i];
     }
 
     retVal = ~retVal;
@@ -144,51 +324,72 @@ char generateChecksum( char* data, int length ) {
     return retVal;
 }
 
-char* loadFileToBuffer() {
-    std::ifstream is ("TestFile", std::ifstream::binary);
+Packet* constructPacket(char* data, int length) {
+    Packet* pPacket = new Packet;
+    static uint8_t sequenceNum = 0;
 
-    if (is) {
-        // get length of file:
-        is.seekg (0, is.end);
-        int length = is.tellg();
-        is.seekg (0, is.beg);
+    pPacket->Sequence = sequenceNum;
 
-        char* cBuff = new char[length];
+    sequenceNum = 1 - sequenceNum;
 
-        std::cout << "Reading " << length << " characters... ";
-        // read data as a block:
-        is.read (cBuff,length);
-
-        if (is)
-          std::cout << "all characters read successfully.\n";
+    for( int i=0; i < DATASIZE; i++ ) {
+        if( i < length )
+            pPacket->Data[i] = data[i];
         else
-          std::cout << "error: only " << is.gcount() << " could be read\n";
-
-        is.close();
-
-        return cBuff;
+            pPacket->Data[i] = '\0';
     }
 
-    return NULL;
+    pPacket->Checksum = generateChecksum(pPacket);
+
+    return pPacket;
 }
 
-Packet* constructPacket(char* data, int length) {
-	Packet* pPacket = new Packet;
-	static uint8_t sequenceNum = 0;
 
-	pPacket->Sequence = sequenceNum;
+bool sendPacket(const Packet* pPacket, bool bLost) {
+    bool bReturn = false;
 
-	sequenceNum = 1 - sequenceNum;
-
-	for( int i=0; i < DATASIZE; i++ ) {
-		if( i < length )
-			pPacket->Data[i] = data[i];
-		else
-			pPacket->Data[i] = '\0';
+	//Send packet
+	if(!bLost)
+	{
+		if (sendto(fd, pPacket, BUFSIZE, 0, (struct sockaddr*)&serverAddress, slen) == -1) {
+			cerr << "Problem sending packet with sequence #" << pPacket->Sequence << "..." << endl;
+			bReturn = false;
+		} 
 	}
 
-	pPacket->Checksum = generateChecksum((char*)pPacket->Data, DATASIZE);
 
-	return pPacket;
+	int recvPollVal = 0;
+	int iLength = 0;
+	struct pollfd ufds;
+	unsigned char recvline[MAXLINE + 1];
+	time_t timer;
+
+	//Wait on return
+	ufds.fd = fd;
+	ufds.events = POLLIN;
+	recvPollVal = poll(&ufds, 1, 20);
+
+	if( recvPollVal == -1 ) {            //If error occurs
+		cerr << "Error polling socket..." << endl;
+		bReturn = false;
+	} else if( recvPollVal == 0 ) {        //If timeout occurs
+		cerr << "Timeout... Lost Packet, Sequence Number - "  << (int)pPacket->Sequence << endl;
+		bReturn = false;
+	} else {
+		iLength = recvfrom(fd, recvline, MAXLINE, 0, (struct sockaddr*)&serverAddress, &slt);
+
+		if( recvline[0] == '1') {          //If ACK Received, return true
+			cout << "ACK - " << (int)recvline[1] << endl;
+			//cout << pPacket->Data << endl;
+		    bReturn = true;
+		} else if( recvline[0] == '0' ) {     //Else if NAK, return false
+			cout << "NAK - " << (int)recvline[1] << endl;
+		    bReturn = false;
+		} else {                             //Else bad stuff, return false
+		    bReturn = false;
+		}
+	}
+  
+	return bReturn;
 }
 
--- proj1/ftp_server.cpp
+++ proj1/ftp_server.cpp
@@ -18,26 +18,41 @@ using namespace std;
 
 #define PORT 10035
 #define BUFSIZE 128
+#define DATASIZE 126
+#define HEADERSIZE 2
+#define MAXLINE 1024
 
 /* PORTS ASSIGNED TO GROUP
 	10034 - 10037 
 */
 
-char generateChecksum( char* data, int length );
+typedef struct {
+    uint8_t Sequence;
+    unsigned char Checksum;
+    char Data[DATASIZE];
+} Packet;
+
+// GLOBAL DATA
+struct sockaddr_in myaddr;      		/* our address */
+struct sockaddr_in remaddr;     		/* remote address */
+socklen_t addrlen = sizeof(remaddr);        /* length of addresses */
+int recvlen;                    		/* # bytes received */
+int fd;                         		/* our socket */
+unsigned char buf[BUFSIZE];    		/* receive buffer */
+uint8_t seqnum;
+char data[BUFSIZE - HEADERSIZE];
+
+// ACK and NAK constants
+char nak[2] = {'0', '0'};
+char ack[2] = {'1', '0'};
+
+Packet* pPacket = new Packet;
+
+unsigned char generateChecksum( Packet*); 
+void receiveData();
 
 int main()
-{
-    struct sockaddr_in myaddr;      				/* our address */
-    struct sockaddr_in remaddr;     				/* remote address */
-    socklen_t addrlen = sizeof(remaddr);            /* length of addresses */
-    int recvlen;                    				/* # bytes received */
-    int fd;                         				/* our socket */
-    unsigned char buf[BUFSIZE];    					/* receive buffer */
-
-    // ACK and NAK constants
-    const char nak[1] = {0};
-    const char ack[1] = {1};
-    
+{    
 
     /* create a UDP socket */
 
@@ -57,50 +72,110 @@ int main()
             perror("bind failed");
             return 0;
     }
+	
+	cout<<"\n- Reliable FTP Application -\n";
+	cout<<"Waiting on port: "<< PORT << "\n";
 
     /* now loop, receiving data */
     for (;;) {
-            recvlen = recvfrom(fd, buf, BUFSIZE, 0, (struct sockaddr *)&remaddr, &addrlen);
+        recvlen = recvfrom(fd, buf, BUFSIZE, 0, (struct sockaddr *)&remaddr, &addrlen);
+
+	    memcpy(pPacket, buf, BUFSIZE);
 
             if (recvlen > 0) {
 
 		//Add data to buffer (minus two byte header)
-                char data[BUFSIZE - 2];
-                for( int x = 2; x < BUFSIZE; x++) {
-                	data[x - 2] = buf[x];
+                for( int x = HEADERSIZE; x < recvlen; x++) {
+                	data[x - HEADERSIZE] = buf[x];
 					cout << buf[x];
                 }
 				cout << endl;
-
+	
 		//Make checksum
-                if ( generateChecksum(data, BUFSIZE - 2) != buf[1]) {
+                if ( generateChecksum(pPacket) != buf[1]) {
 					cout << "Checksum invalid - NAK\n";
-                	sendto(fd, nak, strlen(nak), 0, (struct sockaddr *)&remaddr, addrlen);
+					nak[1] = seqnum;
+                	sendto(fd, nak, 2, 0, (struct sockaddr *)&remaddr, addrlen);
                 }
                 else {
-			cout << "Checksum valid - ACK\n";	
-                	sendto(fd, ack, strlen(ack), 0, (struct sockaddr *)&remaddr, addrlen);
-                }
-            }
+					string command(data);
+					if ( command.substr(0,3) == "PUT" ) {
+						cout << "Checksum valid - ACK\n";
+						seqnum = pPacket->Sequence;
+						ack[1] = seqnum;
+                		sendto(fd, ack, 2, 0, (struct sockaddr *)&remaddr, addrlen);
+						receiveData();
+						cout << "PUT successfully completed" << endl;
+					}
+					else {
+						cout << "Not a PUT command - NAK\n";
+						nak[1] = (char)pPacket->Sequence ^ 30;
+				        sendto(fd, nak, 2, 0, (struct sockaddr *)&remaddr, addrlen);
+					}
+				}
+			}
 
-            // EXAMPLE CODE  
-            // printf("received %d bytes\n", recvlen);
-            // if (recvlen > 0) {
-            //         buf[recvlen] = 0;
-            //         printf("received message: \"%s\"\n", buf);
-            // }
     }
     /* never exits */
 }
 
-char generateChecksum( char* data, int length ) {
-   char retVal = 0x00;
 
-   for( int i=0; i < length; i++ ) {
-       retVal += *(data + i);
-   }
+unsigned char generateChecksum( Packet* pPacket ) {
+    unsigned char retVal = 0x00;
 
-   retVal = ~retVal;
+    retVal = pPacket->Sequence;
 
-   return retVal;
+    for( int i=0; i < DATASIZE; i++ ) {
+        retVal += pPacket->Data[i];
+    }
+
+    retVal = ~retVal;
+
+    return retVal;
 }
+
+void receiveData() {
+	ofstream outFile;
+	outFile.open("TestFile.txt",fstream::out | fstream::trunc);
+	
+	recvlen = recvfrom(fd, buf, BUFSIZE, 0, (struct sockaddr *)&remaddr, &addrlen);
+	
+	memcpy(pPacket, buf, BUFSIZE);
+	
+	while ( recvlen > 1 ) {
+		//Make checksum
+		if ( generateChecksum(pPacket) != buf[1]) {
+			cout << "Checksum invalid - NAK - Sequence Num: " << (int)seqnum << "\n";
+			nak[1] = seqnum;
+			sendto(fd, nak, 2, 0, (struct sockaddr *)&remaddr, addrlen);
+		}
+		else {
+			cout << "ACK - Seq Num: " << (int)seqnum << "\n";
+			ack[1] = seqnum;
+			sendto(fd, ack, 2, 0, (struct sockaddr *)&remaddr, addrlen);
+			//Add data to buffer (minus two byte header)
+			for( int x = HEADERSIZE; x < recvlen; x++) {
+				data[x - HEADERSIZE] = buf[x];
+				if ( x < 50 ) {
+					cout << buf[x];
+				}
+			}
+
+			cout << endl;
+			string buffer(data);
+
+			if ( seqnum != pPacket->Sequence ) {
+				seqnum = pPacket->Sequence;
+				outFile << buffer;
+			}
+		}
+
+		recvlen = recvfrom(fd, buf, BUFSIZE, 0, (struct sockaddr *)&remaddr, &addrlen);
+		memcpy(pPacket, buf, BUFSIZE);
+	}
+
+	ack[1] = seqnum;
+	sendto(fd, ack, 2, 0, (struct sockaddr *)&remaddr, addrlen);
+	outFile.close();
+}
+
